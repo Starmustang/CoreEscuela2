b8278259a1335a999f4c5c1534db626524fca7e7
Wilson <paschendalemaiden@hotmail.com>
2024-05-26 23:06:48 -0400

    *Cree la estructura basica, cree la clase escuela y le agrege ciertas variables *hice que el constructor pida algunos parametros para funcionar

---
+++ Entidades/Escuela.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreEscuela.Entidades
+{
+    internal class Escuela
+    {        
+            public string Nombre { get; set; }
+
+            public int AñoDeCreacion { get; set; }
+
+            public string Pais { get; set; }
+
+            public string Ciudad { get; set; }
+
+        //<summary>
+        //Esta es la forma que estoy acostumbrado en crear los constructores
+        //<summary>
+        /*public Escuela(string nombre, int AñoDeCreacion)
+        //{
+        //    this.Nombre = nombre;
+        //    this.AñoDeCreacion = AñoDeCreacion;
+        //}
+        */
+
+        //<summary>
+        //Esta es la forma reducida de hacer un constructor, similar a una expresion lambda
+        //<summary>
+
+
+        public Escuela(string nombre, int añoDeCreacion) => (Nombre, AñoDeCreacion) = (nombre, añoDeCreacion);
+    }
+
+}
+    
+
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreEscuela.Entidades;
+
+namespace CoreEscuela
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Wilson Academy ", 2001);
+            
+            Console.WriteLine(escuela.Nombre + escuela.AñoDeCreacion);
+            Console.ReadKey();
+        }
+    }
+}
